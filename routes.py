e710cff16fddc344ad64d17fefeb10995c4a893d
Alessio Ragno <spideralessio97@gmail.com>
2024-06-22 12:16:06 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -58,7 +58,7 @@ def vton():
     
     img = request.files['image']
     garm_img = request.files['dress']
-    data = request.form['data']
+    data = request.form
     category = data.get('category', '')
     garment_des = data.get('description', '')
 
